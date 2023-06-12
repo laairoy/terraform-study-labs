8283c4495454e396bf4b0cd868729c52328ab0cd
laairoy <66977800+laairoy@users.noreply.github.com>
2023-06-12 19:20:07 -0300

    Fixed key_name in instance definition

--- aws-vm-vpc-rs/vm.tf
+++ aws-vm-vpc-rs/vm.tf
@@ -6,7 +6,7 @@ resource "aws_key_pair" "key" {
 resource "aws_instance" "vm" {
   ami                         = "ami-024e6efaf93d85776"
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.key
+  key_name                    = aws_key_pair.key.key_name
   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
   associate_public_ip_address = true
